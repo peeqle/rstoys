3c45dba45c933fdfbf24ba29a0de429eff027781
gitler <lobach.artem.v@gmail.com>
2024-08-18 20:36:55 +0200

    s

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ mod lines;
 mod files;
 
 fn main() {
-    asciilator::DefaultImageASCII.create_ascii_image("C:/Users/lobac/Pictures/shrek4_disneyscreencaps.com_675.0 - Copy.jpg", brt::brt_std);
+    
 }
